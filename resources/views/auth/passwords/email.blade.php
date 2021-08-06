2663b270f6eb5a2841b12a8de788dee85b34c56b
Rahmat Subandi <rhmtin12@gmail.com>
2021-08-06 22:44:06 +0700

    change view reset and fixing query env

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mt-8">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
@@ -39,6 +39,11 @@
                         </div>
                     </form>
                 </div>
+            </div>
+            <div class="text-center mt-4">
+                <a href="{{ url('/') }}">
+                    {{ __('Back to Home') }}
+                </a>
             </div>
         </div>
     </div>
